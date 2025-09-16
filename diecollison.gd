46934b96e3ca0e569613f26d48f56feed6ff4610
umbigbry <umbigbrypizza@gmail.com>
2025-09-16 13:19:16 -0700

    reworked grabbing + scale change for everything

--- diecollison.gd
+++ diecollison.gd
@@ -1,5 +1,5 @@
 extends CollisionShape2D
-var grabbed: bool = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,10 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-
 	pass
-
-
-
-func _on_player_grab() -> void:
-	grabbed = true
